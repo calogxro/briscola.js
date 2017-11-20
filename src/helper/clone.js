bebb77c80374477d933fe6afb1b5e7f44e565a0c
calogero <calogero@neuron-webagency.com>
2017-11-20 17:02:40 +0100

    delete all files

--- src/helper/clone.js
+++
@@ -1,9 +0,0 @@
-
-//http://stackoverflow.com/questions/6089058/nodejs-how-to-clone-a-object
-//this is a quick and dirty solution 
-
-function clone(data) {
-  return JSON.parse(JSON.stringify(data));
-}
-
-module.exports = clone;
